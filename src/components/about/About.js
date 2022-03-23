77eedbf1c99c9279127e97c3f319edeb836a614f
albertorg <57998849+albertorg@users.noreply.github.com>
2022-03-23 17:36:40 +0100

    dd

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,54 @@
 import React from 'react'
 import './about.css'
+// import ME from "../../assets/me-about.jpg";
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className='about_container  container header_container'>
+        <div className='about_me'>
+          <div className='about_me-image'>
+            {/* <img src={ ME } alt=" Alberto Rodriguez"/> */}
+          </div>  
+        </div>
+
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>+2 Years Working</small>
+            </article>
+
+            <article className='about_card'>
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>+10 Worlwide</small>
+            </article>
+
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>+4 Complete</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac maximus 
+            ipsum. Sed a vulputate mauris. Etiam quis posuere tellus. Nam vitae 
+            vulputate justo, ac tristique est. Nam vulputate leo.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import { CTA } from "./CTA";
+import { HeaderSocials } from "./HeaderSocials";
+import ME from '../../assets/me.png'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header_container'>
+        <h5>Hello I'm</h5>
+        <h1>Alberto Rodríguez</h1>
+        <h5 className='text-ligth'>Front-End Developer</h5>
+        <CTA /> 
+        <HeaderSocials />
+        
+        <div className='me'>
+          <img src={ ME } alt="alberto rodriguez"/>
+        </div>
+
+      </div> 
+    </header>
   )
 }
 
