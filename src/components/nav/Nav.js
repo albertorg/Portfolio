5ff68a54cb5e5470b6caacfb187ae1356009f18a
albertorg <57998849+albertorg@users.noreply.github.com>
2022-03-31 03:27:44 +0200

    links fixed

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -10,28 +10,29 @@ export const Nav = () => {
   const [open, setOpen] = useState(false)
 
   const hamburgerIcon = <button type="button" className='collapse' onClick={() => setOpen(!open)}>
-                          <GiHamburgerMenu className='collapse_icon' />
-                        </button>
+    <GiHamburgerMenu className='collapse_icon' />
+  </button>
 
   const closeIcon = <button type="button" className='collapse' onClick={() => setOpen(!open)}>
-                      <AiFillCloseCircle className='collapse_icon' />
-                    </button>
+    <AiFillCloseCircle className='collapse_icon' />
+  </button>
 
-  const closeMobileMenu = () => setOpen( false )                  
+
+  const closeMobileMenu = () => setOpen(false)
 
   return (
     <nav className='navbar'>
       <div className='container nav_container'>
-        <div className='logo_container'> 
+        <div className='logo_container'>
           <a href="#header" className='logo'>Alberto Rodríguez</a>
           {open ? closeIcon : hamburgerIcon}
         </div>
 
         <div className='menu_container'>
           <Navigation />
-          {open && <MobileNavigation isMobile={ true } closeMobileMenu={ closeMobileMenu }/>}
+          {open && <MobileNavigation isMobile={true} closeMobileMenu={closeMobileMenu} />}
         </div>
-        
+
 
       </div>
     </nav>
